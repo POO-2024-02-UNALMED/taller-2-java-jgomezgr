2d206a43ea60178d6f4b428b65e6f98ebf7ed5f5
jgomezgr <160145693+jgomezgr@users.noreply.github.com>
2024-11-11 21:19:27 -0500

    Codigo

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,48 @@
+package test;
+
+class Auto {
+    String modelo;
+    int precio;
+    Asiento[] asientos;
+    Motor motor;
+    String marca;
+    int registro;
+    static int cantidadCreados;
+
+    int cantidadAsientos(){
+
+        int numAsientos = 0;
+
+        for(int i = 0; i < asientos.length; i++){
+
+            if(asientos[i] != null){
+
+                numAsientos++;
+
+            }
+        }
+
+        return numAsientos;          
+    }
+
+    String verificarIntegridad(){
+
+        if(this.registro == this.motor.registro){
+
+            for(int i = 0; i < this.asientos.length; i++){
+
+                if(asientos[i] != null){
+
+                    if(asientos[i].registro != this.registro){
+                        return "Las piezas no son originales";
+                    }
+                }
+            }
+            return "El auto es original";
+
+        }
+        return "Las piezas no son originales";
+    }
+
+}
+
